261fbf1855e31c4ad7cd15d4817aecc4ebb216dc
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-06-10 19:35:27 +0400

    new_config

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+import time
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('HelloWorldDatabase')
+
+def lambda_handler(event, context):
+    gmt_time = time.gmtime()
+    now = time.strftime('%a, %d %b %Y %H:%M:%S +0000', gmt_time)
+    name = event['firstName'] +' '+ event['lastName']
+    
+    response = table.put_item(
+        Item={
+            'ID': name,
+            'LatestGreetingTime':now
+            })
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda, ' + name)
+    }
