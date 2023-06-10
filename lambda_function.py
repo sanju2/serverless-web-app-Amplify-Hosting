3648ec9382f24c7688683a0235ba883fcfddec61
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-06-10 21:06:08 +0400

    new_config

--- lambda_function.py
+++ lambda_function.py
@@ -18,5 +18,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda, ' + name)
+        'body': json.dumps('Hello Mr., ' + name)
     }
